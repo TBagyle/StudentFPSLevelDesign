b842f2647d8d05a47a9c1263da7b03cc40c9e36a
TBagyle <stu-357311@gapps.suffolk.ac.uk>
2026-09-11 11:50:36 +0100

    Almost Finished Level
    Close to finishing and ive done some wacky scaliing to the player model and the buttefly skin

---
+++ Assets/DO NOT TOUCH/Scripts/Deathbox.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Deathbox : MonoBehaviour
+{
+    public Scene activeScene;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        activeScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(activeScene.name);
+        }
+    }
+}
